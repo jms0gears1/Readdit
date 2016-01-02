b59b7087ae9ceb682c980d46575e0c0af9bfa4f4
James Maupin <jmsMaupinRC@gmail.com>
2016-01-02 14:42:00 -0500

    Add RecyclerView UI

--- app/src/main/java/com/jamesmaupin/readdit/MainActivity.java
+++ app/src/main/java/com/jamesmaupin/readdit/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -11,21 +13,23 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.rvSubmissions);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new SubmissionAdapter(30);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
--- app/src/main/java/com/jamesmaupin/readdit/SubmissionAdapter.java
+++ app/src/main/java/com/jamesmaupin/readdit/SubmissionAdapter.java
@@ -1,7 +1,37 @@
 package com.jamesmaupin.readdit;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by jmsgears on 1/1/16.
  */
-public class SubmissionAdapter {
+public class SubmissionAdapter extends RecyclerView.Adapter<SubmissionAdapter.ViewHolder> {
+    private int num;
+    public SubmissionAdapter(int i) {
+        num = i;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.submission_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return num;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
