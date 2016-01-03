98d5002b505e22888b73c5a489bcf1443f7aca05
James Maupin <jmsMaupinRC@gmail.com>
2016-01-03 05:25:21 -0500

    Remove Client_id from constants file

--- app/src/main/java/com/jamesmaupin/readdit/Utilities/Constants.java
+++ app/src/main/java/com/jamesmaupin/readdit/Utilities/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public final static String DEVICE_ID                = UUID.randomUUID().toString();
     public final static String BASE_URL                 = "https://www.reddit.com/api/v1/";
     public final static String OAUTH_BASE_URL           = "https://oauth.reddit.com/";
-    public final static String CLIENT_ID                = "65BBTfJCxszCjg";
+    public final static String CLIENT_ID                = "";
     public final static String CREDENTIALS              = CLIENT_ID + ":" + "";
     public final static String REDIRECT_URI             = "http://localhost";
     public final static String GRANT_TYPE_REFRESH       = "refresh_token";
